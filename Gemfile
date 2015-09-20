ee424df347d23babc86a24928a731401455b0123
Steve Musgrave <steve.musgrave@outlook.com>
2015-09-20 21:33:50 +0100

    to install Paperclip and ImageMagick

--- Gemfile
+++ Gemfile
@@ -12,6 +12,7 @@ gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 
 gem 'bootstrap-sass', '~> 3.3.4.1'
+gem "paperclip", "~> 4.3"
 
 
 group :test do
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150920182738) do
+ActiveRecord::Schema.define(version: 20150920201421) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name_english"
@@ -30,6 +30,10 @@ ActiveRecord::Schema.define(version: 20150920182738) do
     t.integer  "weight"
     t.datetime "created_at",          null: false
     t.datetime "updated_at",          null: false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
 end
