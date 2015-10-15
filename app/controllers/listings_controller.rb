2c541583d8f903d9944af0101c50a6501e135fee
Steve Musgrave <steve.musgrave@outlook.com>
2015-10-15 21:59:27 +0100

    to change the listings order to updated_at

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -6,7 +6,8 @@ class ListingsController < ApplicationController
   def index
     if params[:category].blank?
       @title    = "Our Products"
-      @listings = Listing.all.order('random()').paginate(page: params[:page], per_page: 12)
+      # @listings = Listing.all.order('random()').paginate(page: params[:page], per_page: 12)
+      @listings = Listing.all.order("updated_at DESC").paginate(page: params[:page], per_page: 12)
     else
       @category_id = Category.find_by(name_english: params[:category]).id
       @title       = Category.find_by(name_english: params[:category]).name_english
