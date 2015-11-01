933ad5ed56c0c43340e41726851070713527e26a
Steve Musgrave <steve.musgrave@outlook.com>
2015-11-01 12:06:22 +0000

    to change lisitngs view to 6 listings per page and 3 columns wide

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -7,7 +7,8 @@ class ListingsController < ApplicationController
     if params[:category].blank?
       @title    = "Our Products"
       # @listings = Listing.all.order('random()').paginate(page: params[:page], per_page: 12)
-      @listings = Listing.all_cached.order("updated_at DESC").paginate(page: params[:page], per_page: 12)
+      # @listings = Listing.all.order("updated_at DESC").paginate(page: params[:page], per_page: 12)
+      @listings = Listing.all_cached.order("updated_at DESC").paginate(page: params[:page], per_page: 6)
     else
       @category_id = Category.find_by(name_english: params[:category]).id
       @title       = Category.find_by(name_english: params[:category]).name_english
